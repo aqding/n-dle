d13dee0b023b5e25b4701a5e04c82228257856bb
Allen Ding <aqding@mit.edu>
2022-06-01 23:31:33 -0400

    display current word guess and remaining guesses

--- client/src/components/modules/NewWord.js
+++ client/src/components/modules/NewWord.js
@@ -1,7 +1,18 @@
 import React, { useState, useEffect } from "react";
 
 import "./NewWord.css";
-
-const NewWord = ({ word }) => {};
+import LetterBlock from "./LetterBlock";
+const NewWord = ({ word, wordLength }) => {
+  const wordRep = [];
+  for (const letter of word) wordRep.push(letter);
+  while (wordRep.length < wordLength) wordRep.push("");
+  return (
+    <div className="newWordContainer">
+      {wordRep.map((letter) => (
+        <LetterBlock letter={letter} />
+      ))}
+    </div>
+  );
+};
 
 export default NewWord;
