3d874e4e1c3bd944d9fd09d9f66c5f8c18c2c896
Allen Ding <aqding@mit.edu>
2022-06-01 23:31:00 -0400

    display guessed words

--- client/src/components/modules/GuessedWord.js
+++ client/src/components/modules/GuessedWord.js
@@ -1,7 +1,24 @@
 import React, { useState, useEffect } from "react";
 
 import "./GuessedWord.css";
+import LetterBlock from "./LetterBlock";
 
-const GuessedWord = ({ word }) => {};
+const GuessedWord = ({ word, targetWord }) => {
+  const wordRep = [];
+  for (let i = 0; i < word.length; i++) {
+    let color = "";
+    if (word[i] === targetWord[i]) color = "green";
+    else if (targetWord.includes(word[i])) color = "yellow";
+    else color = "grey";
+    wordRep.push({ char: word[i], color: color });
+  }
+  return (
+    <div className="guessedWordContainer">
+      {wordRep.map((letter) => (
+        <LetterBlock letter={letter.char} color={letter.color} />
+      ))}
+    </div>
+  );
+};
 
 export default GuessedWord;
